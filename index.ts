9317ef5590c4e9a2a55f37ba1c468df2464dbc3e
Ahmed Besrour <ahmed.bessrour@gmail.com>
2021-12-18 21:01:37 +0100

    :recycle: Drash land example

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello Tunisian JS Community !");
+import * as Drash from "https://deno.land/x/drash@v2.2.0/mod.ts";
+
+class HomeResource extends Drash.Resource {
+  public paths = ["/"];
+
+  public GET(request: Drash.Request, response: Drash.Response): void {
+    return response.json({
+      hello: "Hello Tunisian JS Community",
+    });
+  }
+}
+
+const server = new Drash.Server({
+  hostname: "0.0.0.0",
+  port: 8000,
+  protocol: "http",
+  resources: [HomeResource],
+});
+
+server.run();
+
+console.log(`Server running at ${server.address}.`);
